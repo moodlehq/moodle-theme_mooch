4df35092b14632bf0ab3adb3dcc89063799d9700
Andrew Davis <andrew@moodle.com>
2015-05-13 14:59:45 +0800

    MDLSITE-4045 added version.php

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+$plugin->version  = 2015051300;   // The (date) version of this module + 2 extra digital for daily versions
+                                  // This version number is displayed into /admin/forms.php
+$plugin->requires = 2014111005;   // Requires at least Moodle 2.8.5  (Build: 20150505)
+                                  // The required version was somewhat arbitrarily chosen when adding version.php
+                                  // theme_mooch may in fact work with earlier versions of Moodle.
+$plugin->cron     = 0;
