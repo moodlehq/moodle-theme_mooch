e6b39e7998c565ffed04f4a35885b348ef0432e4
Dan Poltawski <dan@moodle.com>
2015-11-19 15:12:23 +0000

    Add missing $plugin->component

--- version.php
+++ version.php
@@ -1,5 +1,6 @@
 <?php
 
+$plugin->component = 'theme_mooch';
 $plugin->version  = 2015051300;   // The (date) version of this module + 2 extra digital for daily versions
                                   // This version number is displayed into /admin/forms.php
 $plugin->requires = 2014111005;   // Requires at least Moodle 2.8.5  (Build: 20150505)
