a6fb6891c5e941a8cc5650ee345ca54a570d3254
David Mudrák <david@moodle.com>
2016-01-13 12:52:05 +0100

    Clean up version.php and add explicit dependencies

--- version.php
+++ version.php
@@ -1,9 +1,35 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme for moodle.net (formally hub.moodle.org)
+ *
+ * @package     theme_mooch
+ * @copyright   2011 Marina Glancy <marina@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_mooch';
-$plugin->version  = 2015051300;   // The (date) version of this module + 2 extra digital for daily versions
-                                  // This version number is displayed into /admin/forms.php
-$plugin->requires = 2014111005;   // Requires at least Moodle 2.8.5  (Build: 20150505)
-                                  // The required version was somewhat arbitrarily chosen when adding version.php
-                                  // theme_mooch may in fact work with earlier versions of Moodle.
-$plugin->cron     = 0;
+$plugin->version = 2016011300;
+$plugin->requires = 2014111005;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = [
+    'theme_arialist' => ANY_VERSION,
+    'theme_canvas' => ANY_VERSION,
+    'theme_base' => ANY_VERSION,
+];
